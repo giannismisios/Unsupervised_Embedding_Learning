7472aedff6781213bb368306b8fc8ef0cdfee2ca
giannismisios <giannismisios@gmail.com>
2022-02-06 20:48:32 +0200

    ok

--- BatchAverage.py
+++ BatchAverage.py
@@ -4,39 +4,41 @@ from torch import nn
 import math
 import numpy as np
 
+
 class BatchCriterion(nn.Module):
-    ''' Compute the loss within each batch  
+    ''' Compute the loss within each batch
     '''
+
     def __init__(self, negM, T, batchSize):
         super(BatchCriterion, self).__init__()
         self.negM = negM
         self.T = T
-        self.diag_mat = 1 - torch.eye(batchSize*2).cuda()
-        
+        self.diag_mat = 1 - torch.eye(batchSize * 2).cuda()
+
     def forward(self, x, targets):
         batchSize = x.size(0)
-        
-        #get positive innerproduct
-        reordered_x = torch.cat((x.narrow(0,batchSize//2,batchSize//2),\
-                x.narrow(0,0,batchSize//2)), 0)
-        #reordered_x = reordered_x.data
-        pos = (x*reordered_x.data).sum(1).div_(self.T).exp_()
 
-        #get all innerproduct, remove diag
-        all_prob = torch.mm(x,x.t().data).div_(self.T).exp_()*self.diag_mat
-        if self.negM==1:
+        # get positive innerproduct
+        reordered_x = torch.cat((x.narrow(0, batchSize // 2, batchSize // 2), \
+                                 x.narrow(0, 0, batchSize // 2)), 0)
+        # reordered_x = reordered_x.data
+        pos = (x * reordered_x.data).sum(1).div_(self.T).exp_()
+
+        # get all innerproduct, remove diag
+        all_prob = torch.mm(x, x.t().data).div_(self.T).exp_() * self.diag_mat
+        if self.negM == 1:
             all_div = all_prob.sum(1)
         else:
-            #remove pos for neg
-            all_div = (all_prob.sum(1) - pos)*self.negM + pos
+            # remove pos for neg
+            all_div = (all_prob.sum(1) - pos) * self.negM + pos
 
         lnPmt = torch.div(pos, all_div)
 
         # negative probability
-        Pon_div = all_div.repeat(batchSize,1)
+        Pon_div = all_div.repeat(batchSize, 1)
         lnPon = torch.div(all_prob, Pon_div.t())
         lnPon = -lnPon.add(-1)
-        
+
         # equation 7 in ref. A (NCE paper)
         lnPon.log_()
         # also remove the pos term
@@ -48,5 +50,5 @@ class BatchCriterion(nn.Module):
 
         # negative multiply m
         lnPonsum = lnPonsum * self.negM
-        loss = - (lnPmtsum + lnPonsum)/batchSize
+        loss = - (lnPmtsum + lnPonsum) / batchSize
         return loss
